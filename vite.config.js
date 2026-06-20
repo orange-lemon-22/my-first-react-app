88fd915920fafdb9930b1b39628e933f00747257
orange-lemon-22 <orange-lemon@tomorrow-enjoyed.com>
2026-06-20 13:56:11 +0900

    Configure Vite base path and GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-first-react-app/', // 👈 リポジトリ名を指定してURLのズレを防ぐ
 })
